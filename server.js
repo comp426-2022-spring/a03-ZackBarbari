c971ec62c06b46872d410d1ca9d7d40c7931ce7e
Zack Barbari <zackb@unc.edu>
2022-04-02 15:44:02 -0400

    endpoint added

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
 });
 
+app.use(function(req, res){
+  res.status(404).send('404 NOT FOUND')
+});
+
+app.get('/app/', (req, res) => {
+  // Respond with status 200
+    res.statusCode = 200;
+  // Respond with status message "OK"
+      res.statusMessage = 'OK';
+      res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+      res.end(res.statusCode+ ' ' +res.statusMessage)
+});
+
 function coinFlip() {
     if (Math.random() >= 0.5) {
       return "heads"
