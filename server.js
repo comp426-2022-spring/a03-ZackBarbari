3144d1069870a2450fecd5ac45ce040f8b5b0707
Zack Barbari <zackb@unc.edu>
2022-04-02 16:09:55 -0400

    endpoint bug

--- server.js
+++ server.js
@@ -4,22 +4,21 @@ const args = require('minimist')(process.argv.slice(2))
 
 args["port"]
 
-const port = args.port || process.env.PORT || 5000
+const port = args.port || process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',port))
 });
 
 app.use(function(req, res){
-  res.status(404).send('404 NOT FOUND')
+  res.status(404).send('404 NOT FOUND');
 });
 
 app.get('/app/', (req, res) => {
       res.statusCode = 200;
       res.statusMessage = 'OK';
-      res.status(statusCode).send(statusMessage)
-      res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-      res.end(res.statusCode+ ' ' +res.statusMessage)
+      res.status(statusCode).end(res.statusCode+ ' ' +res.statusMessage)
+      res.type("text/plain")
 });
 
 function coinFlip() {
