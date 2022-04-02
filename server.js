37accfa9a355c4991045b965823771132100dffa
Zack Barbari <zackb@unc.edu>
2022-04-02 15:03:31 -0400

    port added

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const args = require('minimist')(process.argv.slice(2))
+
+args["HTTP_PORT"]
+
+const HTTP_PORT = args.port || process.env.PORT || 5000
+
+const server = app.listen(HTTP_PORT, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+});
+
+function coinFlip() {
+    if (Math.random() >= 0.5) {
+      return "heads"
+    } else {
+      return "tails"
+    }
+}
+  
+function coinFlips(flips) {
+    const values = []
+    for (let i = 0; i < flips; i++) {
+      values[i] = coinFlip()
+    }
+    return values
+}
+  
+function countFlips(array) {
+    let count = { heads: 0, tails: 0 }
+  array.forEach(element => 
+    { if (element == "heads") {
+      count.heads += 1
+    } else {
+      count.tails += 1
+    }
+   })
+   return count
+}
+  
+function flipACoin(call) {
+    let coin = { call: call, flip: "NULL", result: "NULL"}
+    let flip = coinFlip()
+    coin.flip = flip
+    if (call == flip) {
+      coin.result = "win"
+    } else {
+      coin.result = "lose"
+    }
+    return coin
+}
